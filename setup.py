d93d8682cab77fd79bb71c0bdef5d5c2c81c5cd4
klaifer <klaifer.garcia@gmail.com>
2021-08-04 12:10:06 -0300

    Link to github

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="wikitojson",
-    version="0.0.1",
+    version="0.0.2",
     author="Klaifer Garcia",
     description="Wikipedia(English) to json",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/Klaifer/wikitojson",
     packages=[],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
