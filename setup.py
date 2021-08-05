5dba36cf4942ffcbb911c47f4a1f9c3ed2a9d232
klaifer <klaifer.garcia@gmail.com>
2021-08-05 10:05:44 -0300

    Next version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="wikitojson",
-    version="0.0.2",
+    version="0.0.3",
     author="Klaifer Garcia",
     description="Wikipedia(English) to json",
     long_description=long_description,
